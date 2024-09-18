771ce953c92b547e4302adf38909ffda02188eaa
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-09-18 12:23:10 -0400

    .

---
+++ hello-world/helloworld/users/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+# Create your views here.
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = UserCreationForm()
+    return render(request, "users/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        # return redirect("users:login")
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+
+            return redirect("posts:list")
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "users/login.html", {"form": form})
