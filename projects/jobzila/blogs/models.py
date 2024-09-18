798cb90a5655fe66fc10b8272ff0420d35ee6405
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-09-18 11:10:59 -0400

    .

--- projects/jobzila/blogs/models.py
+++ projects/jobzila/blogs/models.py
@@ -8,6 +8,8 @@ class Blog(models.Model):
     slug = models.SlugField()
     link = models.URLField()
     date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(default="Sadeed", max_length=40)
+    banner = models.ImageField(default="", blank="True")
 
     def __str__(self):
         return self.title
--- projects/jobzila/jobzila/urls.py
+++ projects/jobzila/jobzila/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -29,3 +32,4 @@ urlpatterns = [
     path("contactus/", views.contactus),
     path("blogs/", include("blogs.urls")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
