3f9de1c63b7328c41fac70c1a788aa60636ed1e3
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-09-18 12:51:51 -0400

    authentication done

--- hello-world/helloworld/users/views.py
+++ hello-world/helloworld/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -7,7 +8,7 @@ def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("posts:list")
     else:
         form = UserCreationForm()
@@ -19,7 +20,7 @@ def login_view(request):
         # return redirect("users:login")
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-
+            login(request, form.get_user())
             return redirect("posts:list")
 
     else:
