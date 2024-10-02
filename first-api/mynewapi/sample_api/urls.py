082009b518ccdfb7f1d7f89beb1dc942978a60d4
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-10-02 12:38:35 -0400

    .

--- first-api/mynewapi/sample_api/urls.py
+++ first-api/mynewapi/sample_api/urls.py
@@ -1,4 +1,8 @@
 from .views import StudentView
 from django.urls import path
 
-urlpatterns = [path("basic/", StudentView.as_view())]
+urlpatterns = [
+    path("basic/", StudentView.as_view()),
+    path("basic/<int:id>/", StudentView.as_view()),
+    path("basic/<int:id>/update/", StudentView.as_view()),
+]
--- first-api/mynewapi/sample_api/views.py
+++ first-api/mynewapi/sample_api/views.py
@@ -4,16 +4,47 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Students
 from .serializers import StudentSerializer
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
 
 class StudentView(APIView):
 
-    def get(self, request, *args, **kwargs):
-        result = Students.objects.all()
-        serializers = StudentSerializer(result, many=True)
-        return Response({"status": "success", "students": serializers.data}, status=200)
+    # def get(self, request, id):
+    #     result = Students.objects.get(id=id)
+    #     if id:
+    #         serializers = StudentSerializer(result)
+    #         return Response(
+    #             {"success": "success", "students": serializers.data}, status=200
+    #         )
+
+    #     result = Students.objects.all()
+    #     serializers = StudentSerializer(result, many=True)
+    #     return Response({"status": "success", "students": serializers.data}, status=200)
+    def get(self, request, id=None):
+        if id:
+            try:
+                result = Students.objects.get(id=id)
+                serializers = StudentSerializer(result)
+                return Response(
+                    {"success": "success", "students": serializers.data}, status=200
+                )
+            except Students.DoesNotExist:
+                return Response(
+                    {"status": "error", "message": "Student not found"}, status=404
+                )
+        else:
+            result = Students.objects.all()
+            serializers = StudentSerializer(result, many=True)
+            return Response(
+                {"status": "success", "students": serializers.data}, status=200
+            )
+
+    # def get(self, request, *args, **kwargs):
+    #     result = Students.objects.all()
+    #     serializers = StudentSerializer(result, many=True)
+    #     return Response({"status": "success", "students": serializers.data}, status=200)
 
     def post(self, request):
         serializer = StudentSerializer(data=request.data)
@@ -28,3 +59,17 @@ class StudentView(APIView):
                 {"status": "error", "data": serializer.errors},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+    def patch(self, request, id):
+        result = Students.objects.get(id=id)
+        serializer = StudentSerializer(result, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+        else:
+            return Response({"status": "error", "data": serializer.errors})
+
+    def delete(self, request, id=None):
+        result = get_object_or_404(Students, id=id)
+        result.delete()
+        return Response({"status": "success", "data": "Record Deleted"})
